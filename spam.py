f85292856c46a42e2b5224b7baf834b3fc36751c
Bryan He <bryanhe@stanford.edu>
2016-10-12 20:20:12 -0700

    Add code

---
+++ spam.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import slacker
+import sys
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Spam some jokers.")
+    parser.add_argument('token', help="Slack API Token (can be obtained from https://api.slack.com/docs/oauth-test-tokens)")
+    args = parser.parse_args()
+    token = args.token
+
+    slack = slacker.Slacker(token)
+
+    users = slack.users.list().body["members"]
+    mpim_users = []
+    for u in users:
+        if u["name"] == "thodrek" or u["name"] == "cdesa":
+            mpim_users.append(u["id"])
+
+    channel = slack.mpim.open(mpim_users).body["group"]["id"]
+    #slack.chat.post_message(channel, "any updates? :slightly_smiling_face:")
+    oldest = "1476328589.000033"
+    while True:
+        mpim = slack.mpim.history(channel, oldest=oldest).body
+        messages = mpim["messages"]
+        if messages != []:
+            oldest = messages[0]["ts"]
+        for m in messages:
+            print(m["ts"])
+            is_human = "user" in m.keys() # Bots have a "username" but no "user"
+            if is_human:
+                if "update" in m["text"].lower():
+                    pass
+                    #slack.chat.post_message(channel, "any updates? :slightly_smiling_face:")
+
+
+
