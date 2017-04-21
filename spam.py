f460c9336369edc7e89136475ccc494732a5671a
Bryan He <bryanhe@stanford.edu>
2017-04-20 21:39:16 -0700

    spam machine

---
+++ spam.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import slacker
+import sys
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Spam some jokers.")
+    parser.add_argument('token', help="Slack API Token (can be obtained from https://api.slack.com/docs/oauth-test-tokens)")
+    args = parser.parse_args()
+    token = args.token
+
+    slack = slacker.Slacker(token)
+
+    users = slack.users.list().body["members"]
+    mpim_users = []
+    for u in users:
+        if u["name"] == "paroma":
+            print("DEBUG")
+            mpim_users.append(u["id"])
+            channel = slack.im.open(u["id"])
+            for i in range(1000000):
+                print(i)
+                slack.chat.post_message(u["id"], ":coral:")
+
+
+
+
